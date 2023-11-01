089509f8234d62e9e7e5a5653db6bcd3ea331e11
itsskey <109790338+itsskey@users.noreply.github.com>
2023-11-01 21:27:38 +0300

    Update 3.2.py

--- 3/3.2.py
+++ 3/3.2.py
@@ -1,18 +1,14 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(first, second, sep_=','):
-
     first = first.split(sep_)
-    array_of_people = []
-    for i in first:
-        if second.find(i) != -1:
-            array_of_people.append(i)
-    return array_of_people
-
+    second = second.split(sep_)
+    intersection_group = set(first).intersection(second)
+    intersection_group = list(intersection_group)
+    intersection_group.sort()
+    return intersection_group
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-
 answer = find_common_participants(participants_first_group, participants_second_group, '|')
-answer.sort()
 print(answer)
 # TODO Провеьте работу функции с разделителем отличным от запятой
